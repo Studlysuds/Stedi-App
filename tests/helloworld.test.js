f65717fed80dac168c10a3a2c163fb9a4adfa29b
Micah Sudweeks <micahsudweeks@gmail.com>
2023-05-18 11:31:49 -0600

    Changed the manual test to an automatic test

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -1,5 +1,9 @@
 import hello from '../utils/helloworld.js';
+import assert from 'assert';
 
 it ("Should say hello", ()=>{
-    hello();
-})
+    const hi = hello();
+    // console.log("This is the value of " + hi)
+    assert.equal(hi,"hello");
+
+});
